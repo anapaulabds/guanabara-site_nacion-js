0ba670cc8eb9e9771af008c5266dbf4ea88e99c1
Ana <anappa08@gmail.com>
2021-08-29 20:42:52 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+Este projeto foi realizado no Curso em Vídeo (Guanabara);
+
+A partir de um sistema criado em JavaScript que indentifica a dualidade Brasil-Estrangeiro, precisávamos criar um site que se ligasse as informações e pudesse ser disponibilizada.
+
+Criei o site em HTML5 e harmonizei, de uma forma simples, com CSS3, ligando os dois elementos à linguagem JS!
+
---
+++ script.js
@@ -0,0 +1,9 @@
+function calcular() {
+  var caixa = document.querySelector("#country");
+  var res = document.querySelector("#res");
+  var país = caixa.value;
+  res.innerHTML = `<p>Seu país é <strong>${país.toUpperCase()}</strong>, você é brasileiro!</p>`;
+  if (país != 'Brasil') {
+    res.innerHTML = `<p>Seu país é <strong>${país.toUpperCase()}</strong>, você é estrangeiro!</p>`;
+  }
+}
