3aac569326dcacff155721b1b4f6cee83608bc09
anapaulabds <61979726+anapaulabds@users.noreply.github.com>
2021-08-30 15:30:00 -0300

    adição de operador lógico

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function calcular() {
   var caixa = document.querySelector("#country");
   var res = document.querySelector("#res");
   var país = caixa.value;
-  res.innerHTML = `<p>Seu país é <strong>${país.toUpperCase()}</strong>, você é brasileiro!</p>`;
-  if (país != 'Brasil' || país != 'brasil') {
-    res.innerHTML = `<p>Seu país é <strong>${país.toUpperCase()}</strong>, você é estrangeiro!</p>`;
+  res.innerHTML = `<p>Seu país é <strong>${país.toUpperCase()}</strong>, você é estrangeira(o)!</p>`;
+  if (país == 'Brasil' || país == 'brasil') {
+    res.innerHTML = `<p>Seu país é o <strong>${país.toUpperCase()}</strong>, você é brasileira(o)!</p>`;
   }
 }
